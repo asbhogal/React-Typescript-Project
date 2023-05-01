04fa5738057a7cdb3828d667165ea75c17d8c4ba
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-05-01 16:16:15 +0100

    added generics / functional components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Counter from './components/Counter'
 import Heading from './components/Heading'
+import List from './components/List'
 import Section from './components/Section'
 import { useState } from 'react'
 
@@ -13,6 +14,7 @@ function App() {
       <Heading title={ "Test" }/>
       <Section title="Second Subheading">Section Text</Section>
       <Counter setCount={ setCount }>Count is { count }</Counter>
+      <List items={ ["coffee", "jazz", "code"] } render={ (item: string) => <span className="bold">{ item }</span> }></List>
     </>
   )
 }
